0c52d0a9810f8b558daccec1b916f285e960043d
Damien DEMONAZ <damien.demonaz@gmail.com>
2018-05-23 23:11:13 +0200

    Ajout des droits sur la table lieu pour l'utilisateur apache

--- db_sql/natation_apache_user.sql
+++ db_sql/natation_apache_user.sql
@@ -1,8 +1,8 @@
 CREATE USER apache WITH PASSWORD 'apache';
 
-GRANT EXECUTE ON FUNCTION public.armor(bytea) TO apache;
-
 GRANT EXECUTE ON FUNCTION public.armor(bytea, text[], text[]) TO apache;
+
+GRANT EXECUTE ON FUNCTION public.armor(bytea) TO apache;
 
 GRANT EXECUTE ON FUNCTION public.checkvalue_different(tocheck anyelement, comparison anyelement, onexceptiontext character varying) TO apache;
 
@@ -36,9 +36,9 @@ GRANT EXECUTE ON FUNCTION public.getjugescompetition(idcompetition integer) TO a
 
 GRANT EXECUTE ON FUNCTION public.getjugesequipe(idequipe integer) TO apache;
 
-GRANT EXECUTE ON FUNCTION public.hmac(bytea, bytea, text) TO apache;
-
 GRANT EXECUTE ON FUNCTION public.hmac(text, text, text) TO apache;
+
+GRANT EXECUTE ON FUNCTION public.hmac(bytea, bytea, text) TO apache;
 
 GRANT EXECUTE ON FUNCTION public.login(adressemail character varying, motdepasse character varying) TO apache;
 
@@ -46,37 +46,37 @@ GRANT EXECUTE ON FUNCTION public.pgp_armor_headers(text, OUT key text, OUT value
 
 GRANT EXECUTE ON FUNCTION public.pgp_key_id(bytea) TO apache;
 
-GRANT EXECUTE ON FUNCTION public.pgp_pub_decrypt(bytea, bytea, text) TO apache;
-
 GRANT EXECUTE ON FUNCTION public.pgp_pub_decrypt(bytea, bytea, text, text) TO apache;
 
-GRANT EXECUTE ON FUNCTION public.pgp_pub_decrypt(bytea, bytea) TO apache;
+GRANT EXECUTE ON FUNCTION public.pgp_pub_decrypt(bytea, bytea, text) TO apache;
 
-GRANT EXECUTE ON FUNCTION public.pgp_pub_decrypt_bytea(bytea, bytea, text, text) TO apache;
+GRANT EXECUTE ON FUNCTION public.pgp_pub_decrypt(bytea, bytea) TO apache;
 
 GRANT EXECUTE ON FUNCTION public.pgp_pub_decrypt_bytea(bytea, bytea) TO apache;
 
 GRANT EXECUTE ON FUNCTION public.pgp_pub_decrypt_bytea(bytea, bytea, text) TO apache;
 
+GRANT EXECUTE ON FUNCTION public.pgp_pub_decrypt_bytea(bytea, bytea, text, text) TO apache;
+
 GRANT EXECUTE ON FUNCTION public.pgp_pub_encrypt(text, bytea, text) TO apache;
 
 GRANT EXECUTE ON FUNCTION public.pgp_pub_encrypt(text, bytea) TO apache;
 
-GRANT EXECUTE ON FUNCTION public.pgp_pub_encrypt_bytea(bytea, bytea) TO apache;
-
 GRANT EXECUTE ON FUNCTION public.pgp_pub_encrypt_bytea(bytea, bytea, text) TO apache;
 
-GRANT EXECUTE ON FUNCTION public.pgp_sym_decrypt(bytea, text, text) TO apache;
+GRANT EXECUTE ON FUNCTION public.pgp_pub_encrypt_bytea(bytea, bytea) TO apache;
 
 GRANT EXECUTE ON FUNCTION public.pgp_sym_decrypt(bytea, text) TO apache;
+
+GRANT EXECUTE ON FUNCTION public.pgp_sym_decrypt(bytea, text, text) TO apache;
 
 GRANT EXECUTE ON FUNCTION public.pgp_sym_decrypt_bytea(bytea, text, text) TO apache;
 
 GRANT EXECUTE ON FUNCTION public.pgp_sym_decrypt_bytea(bytea, text) TO apache;
 
-GRANT EXECUTE ON FUNCTION public.pgp_sym_encrypt(text, text) TO apache;
-
 GRANT EXECUTE ON FUNCTION public.pgp_sym_encrypt(text, text, text) TO apache;
+
+GRANT EXECUTE ON FUNCTION public.pgp_sym_encrypt(text, text) TO apache;
 
 GRANT EXECUTE ON FUNCTION public.pgp_sym_encrypt_bytea(bytea, text) TO apache;
 
@@ -118,6 +118,8 @@ GRANT ALL ON SEQUENCE public.seq_equipe_id TO apache;
 
 GRANT ALL ON SEQUENCE public.seq_jugecompetition_id TO apache;
 
+GRANT ALL ON SEQUENCE public.seq_lieu_id TO apache;
+
 GRANT ALL ON SEQUENCE public.seq_personne_id TO apache;
 
 GRANT ALL ON SEQUENCE public.seq_typejuge_id TO apache;
@@ -140,6 +142,8 @@ GRANT ALL ON TABLE public.equipe_personne TO apache;
 
 GRANT ALL ON TABLE public.jugecompetition TO apache;
 
+GRANT ALL ON TABLE public.lieu TO apache;
+
 GRANT ALL ON TABLE public.personne TO apache;
 
 GRANT ALL ON TABLE public.typejuge TO apache;
@@ -158,8 +162,11 @@ GRANT ALL ON TABLE public.all_juge_competition_notes TO apache;
 
 GRANT ALL ON TABLE public.all_nageur_club TO apache;
 
+GRANT ALL ON TABLE public.all_notes TO apache;
+
 GRANT ALL ON TABLE public.all_personne TO apache;
 
 GRANT ALL ON TABLE public.juge_competition TO apache;
 
 GRANT ALL ON TABLE public.juge_competition_agg TO apache;
+
